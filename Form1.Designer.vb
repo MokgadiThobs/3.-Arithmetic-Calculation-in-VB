b956a36ca4dfd74aff7d944f19b579c920485250
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-03 20:20:22 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BtnAdd = New Button()
+        SuspendLayout()
+        ' 
+        ' BtnAdd
+        ' 
+        BtnAdd.Location = New Point(232, 134)
+        BtnAdd.Name = "BtnAdd"
+        BtnAdd.Size = New Size(75, 23)
+        BtnAdd.TabIndex = 0
+        BtnAdd.Text = "Add"
+        BtnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(BtnAdd)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents BtnAdd As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        MsgBox("2 + 2 = " & 2 + 2)
+        MsgBox("2 + 2 = " & 2 - 2)
+        MsgBox("2 + 2 = " & 2 * 2)
+        MsgBox("2 + 2 = " & 2 / 2)
+    End Sub
+End Class
